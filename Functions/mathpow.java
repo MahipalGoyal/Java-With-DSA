1124a57526ed2b24d64b0c0ad885f9848f45af54
MahipalGoyal <Goyalmahipal089@gmail.com>
2026-02-12 07:20:56 +0530

    Delete Functions directory

--- Functions/mathpow.java
+++
@@ -1,7 +0,0 @@
-public class mathpow {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 5;
-        System.out.println("the pow is " + Math.pow(a, b));
-    }
-}
--- Functions/mathsqrt.java
+++
@@ -1,6 +0,0 @@
-public class mathsqrt {
-    public static void main(String[] args) {
-        int a = 269;
-        System.out.println("The square root of " + a + " is " + Math.sqrt(a));
-    }
-}
